f9a61b4bffbe9f0a5ff62b5c0f5d8d52c8435baa
Natthawut79 <koninw1615@gmail.com>
2025-09-09 21:37:38 +0700

    gay

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-    $conn = mysqli_connect('localhost', 'root', '', 'cgameshop') or die('connection failed!');
-
-?>
--- script.js
+++
@@ -1,8 +0,0 @@
-let accountBox = document.querySelector(".header .account-box");
-
-document.querySelector("#user-btn").onclick = () => {
-
-    accountBox.classList.toggle('active')
-    
-}
-
