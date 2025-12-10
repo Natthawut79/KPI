fe3cb2429da69de13e14ef983e06094ea5652860
Natthawut79 <koninw1615@gmail.com>
2025-12-10 13:53:43 +0700

    จัดตำแหน่งTAB2

--- individual_kpi.php
+++ individual_kpi.php
@@ -810,7 +810,7 @@ if (empty($actual_work_all_year_value)) {
                                     <tr data-topic-id="<?php echo $topic_id; ?>"
                                         data-weight="<?php echo htmlspecialchars($item['weight']); ?>"
                                         data-kpi-type-id="<?php echo $kpi_type_id; ?>">
-                                        <td><?php echo htmlspecialchars($item['id']); ?></td>
+                                        <td class="text-left" style="vertical-align: top;"><?php echo htmlspecialchars($item['id']); ?></td>
                                         <td class="text-left" style="vertical-align: top;">
                                             <?php echo htmlspecialchars($item['name']); ?>
                                             <?php if (isset($item['Additional']) && strtoupper($item['Additional']) == 'YES'): ?>
